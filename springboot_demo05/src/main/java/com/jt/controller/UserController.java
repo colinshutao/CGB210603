08e9d58cd70a675f78679d8b401f66ef9c0094d9
zst <zhoushutao521@gmail.com>
2021-08-24 20:29:39 +0800

    test3

---
+++ springboot_demo05/src/main/java/com/jt/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.jt.controller;
+
+import com.jt.pojo.User;
+import com.jt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+@RequestMapping("aaa")
+    public List<User> sadasd() {
+        return userService.selectAll();
+    }
+}
